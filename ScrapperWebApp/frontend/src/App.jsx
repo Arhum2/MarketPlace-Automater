c49bcfdcc848152a71f397c2ce53c445ebcdb6e8
arhum2 <arhum.shahzad@mail.utoronto.ca>
2026-04-11 21:42:17 -0400

    fixed blank page issue

--- ScrapperWebApp/frontend/src/App.jsx
+++ ScrapperWebApp/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProductsPage from './pages/ProductsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductsPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
     </Routes>
   )
 }
